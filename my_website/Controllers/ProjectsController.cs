92838a2921002c6d368304d49090880bea414fc6
keraj37 <jerry.switalski@gmail.com>
2017-04-29 19:02:20 +0200

    p

--- my_website/Controllers/ProjectsController.cs
+++ my_website/Controllers/ProjectsController.cs
@@ -10,6 +10,7 @@ namespace my_website.Controllers
     public class ProjectsController : Controller
     {
         private const string CONSOLE = "console";
+        private const string AI_NAME = "ai";
 
         public ActionResult Index()
         {
@@ -26,7 +27,7 @@ namespace my_website.Controllers
         [ValidateInput(false)]
         public ActionResult Console(string cmd)
         {
-            Session[CONSOLE] += AddNewLine() + string.Format("[{0}@jerryswitalski.com ~] ", User.Identity.Name) + cmd;
+            Session[CONSOLE] += AddNewLine(Session[CONSOLE]) + string.Format("[{0}@jerryswitalski.com ~] ", User.Identity.Name) + cmd;
 
             switch(cmd)
             {
@@ -44,10 +45,38 @@ namespace my_website.Controllers
             return RedirectToAction("Console");
         }
 
-        [NonAction]
-        private string AddNewLine()
+        [HttpGet]
+        public ActionResult AI()
         {
-            return Session[CONSOLE] != null && Session[CONSOLE].ToString() != string.Empty ? "\n" : string.Empty;
+            return View(Session[AI_NAME]);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AI(string cmd)
+        {
+            Session[AI_NAME] += AddNewLine(Session[AI_NAME]) + string.Format("[{0}@jerryswitalski.com ~] ", User.Identity.Name) + cmd;
+
+            switch (cmd)
+            {
+                case "clear":
+                    Session[AI_NAME] = null;
+                    break;
+            }
+
+            string result = Controllers.Console.ConsoleCommandParser.Parse(cmd);
+            if (!string.IsNullOrEmpty(result))
+            {
+                Session[AI_NAME] += "\n" + result;
+            }
+
+            return RedirectToAction("AI");
+        }
+
+        [NonAction]
+        private string AddNewLine(object value)
+        {
+            return !string.IsNullOrEmpty(value as string) ? "\n" : string.Empty;
         }
     }
 }
