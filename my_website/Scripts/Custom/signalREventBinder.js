3ab851091f7814aa7ba9a824f8ccab81eef4c920
Jerry Switalski <jerry.switalski@gmail.com>
2017-06-21 18:15:21 +0200

    fix

--- my_website/Scripts/Custom/signalREventBinder.js
+++
@@ -1,9 +0,0 @@
-﻿var generalHub = $.connection.GeneralHub;
-
-$(function () {
-    if (typeof triggerAndNewMessageToPage !== 'undefined' && triggerAndNewMessageToPage)
-        generalHub.client.addNewMessageToPage = triggerAndNewMessageToPage;
-
-    if (typeof triggerSpreadVisit !== 'undefined' && triggerSpreadVisit)
-        generalHub.client.spreadVisit = triggerSpreadVisit;
-});
--- my_website/Scripts/Custom/visit.js
+++ my_website/Scripts/Custom/visit.js
@@ -2,11 +2,16 @@
     generalHub.server.logVisit();
 }
 
-function triggerSpreadVisit(ip) {
-    $('#visitors').append('<li>Detected visit from: <strong>' + ip + '</strong></li>');
-};
-
 $(function () {
+    generalHub.client.spreadVisit = function (ip) {
+        $('#visitors').append('<li>Detected visit from: <strong>' + ip + '</strong></li>');
+    };
+
+    if (typeof triggerAndNewMessagetopage !== 'undefined' && triggerAndNewMessagetopage)
+        generalHub.client.addNewMessageToPage = triggerAndNewMessagetopage;
+
+    //$.connection.hub.logging = true;
+
     $.connection.hub.start().done(function () {
         sendMessageVisit();
     });
